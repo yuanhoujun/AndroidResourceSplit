bb5b8d23f2a66ea6fc22362985bcbb89638b31d0
scott <626306805@qq.com>
2016-07-26 20:38:51 +0800

    Init commit

---
+++ app/src/main/java/com/scott/app/resourcesplit/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.scott.app.resourcesplit.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.scott.app.resourcesplit.R;
+
+/**
+ * @author Scott Smith @Date 2016年07月16/7/26日 20:32
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
